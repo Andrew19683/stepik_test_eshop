c1168d27c9b935e032a4a95cc1cfc78de2e0f5f2
Andrew19683 <p3orpheus71@gmail.com>
2026-05-17 13:18:56 +0300

    comment time.spleep

--- test_items.py
+++ test_items.py
@@ -5,6 +5,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_has_add_to_cart_button(browser):
     browser.get(link)
-    time.sleep(30) # можно убедитьтся, что язык соответствует выбранному
+    # time.sleep(30) # можно убедитьтся, что язык соответствует выбранному
     add_to_cart_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert add_to_cart_button, "Button 'Add to cart' is not found on the page"
